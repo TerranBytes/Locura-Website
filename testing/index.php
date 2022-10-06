ebeecd85397954c05ae71926db748f56390ed0bd
Credence <erichamby83@gmail.com>
2022-10-05 21:51:26 -0400

    Update index.php

--- testing/index.php
+++ testing/index.php
@@ -1,5 +1,7 @@
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="script.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 </head>
 <nav>
   <ul>
