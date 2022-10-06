905b9c5261e5a79321e60c34c4985dfbb568c38e
Credence <erichamby83@gmail.com>
2022-10-05 21:47:09 -0400

    Create index.php

---
+++ testing/index.php
@@ -0,0 +1,16 @@
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+</head>
+<nav>
+  <ul>
+    <li><a href="Salfia.co">Home</a></li>
+    <li>About me</li>
+    <li>Portfolio</li>
+    <li>Blog</li>
+    <li>Contact me</li>
+    <li class="slide"></li>
+  </ul>
+</nav>
+<section>
+  <div class="title"><span>Jack Salfia</span><span>Graphic Designer and<a target="_blanck" href="salfia.com">Web Developer</a></span></div>
+</section>
